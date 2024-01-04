5b380d0e94f693ab163dbc0426ccc5235217a992
DaisyCorrea <daisy_jch@hotmail.com>
2024-01-03 22:08:40 -0500

    Commit final

--- src/analyzer.js
+++ src/analyzer.js
@@ -23,8 +23,6 @@ const analyzer = {
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
     const caractSinEspaciosIyF = text.trim();
-    //const caracSinEspaciosySignos = caractSinEspaciosIyF.replace(/^\w\s/g, '');
-    // eslint-disable-next-line no-useless-escape
     const caracSinEspaciosySignos = caractSinEspaciosIyF.replace(/[ \.,;'\-\(\)\[\]{}!¡¿\?\:]/g, '');
     const caracExcEspySig = caracSinEspaciosySignos.length;
     return caracExcEspySig;
@@ -58,9 +56,6 @@ const analyzer = {
 
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    // const contadorNumeros = text;
-    // const rContNumer = contadorNumeros.trim().replace(/\D/g, "");
-    // return rContNumer.length;
     const numerosEncontrados = text.match(/\b\d+(?:[.,]\d+)?\b/g)
     if(!numerosEncontrados){
       return 0;
@@ -73,20 +68,6 @@ const analyzer = {
 
   },
   getNumberSum: (text) => {
-    // const palabras = text.split(' ')
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-    // let sumaDigitos = 0;
-    // for (let i = 0; i < palabras.length; i++) {
-    // if (text[i] >= '0' && text[i] <= '9') {
-    //   sumaDigitos += parseInt(text[i], 10);
-    // }
-
-    // match(/\b\d+(?:[.,]\d+)?\b/g)
-    // if(Number(palabras[i])){
-    //     sumaDigitos += parseFloat(palabras[i])
-    //   }
-    // }
-    // return sumaDigitos;
     const numerosEncontrados = text.match(/\b\d+(?:[.,]\d+)?\b/g)
     if(!numerosEncontrados){
       return 0;
